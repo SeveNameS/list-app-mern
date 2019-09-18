081d177a6e7255d73128120ee93ad1ce20720890
SeveNameS <777bpr@gmail.com>
2019-09-19 01:14:06 +0800

    first commit

---
+++ client/src/components/create-user.component.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class CreateUser extends Component {
+
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChangeUsername = this.onChangeUsername.bind(this)
+    this.state = {
+      username: '',
+    }
+  }
+
+  onChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      username: this.state.username
+    }
+
+    console.log(user)
+
+    axios.post('https://mern-stack-app-b.herokuapp.com/users/add', user)
+    .then(res => console.log(res.data))
+
+    this.setState({
+      username: ''
+    })
+
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>Cretae New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username:</label>
+            <input required type="text" className="form-control"
+            value={this.state.username}
+            onChange={this.onChangeUsername}></input>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary"/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose
+    .connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB database connection successfull!"))
+    .catch(err => console.log(err)
+);
+
+const exercisesRouter = require('routes/exercises');
+const usersRouter = require('routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
